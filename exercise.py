b863b1315f51dc6e8ed67ed1f96f4d27a00be1ae
Yaseen Quraishi <yaseen.quraishi@digital.hmrc.gov.uk>
2017-12-19 16:00:20 +0000

    exercises

---
+++ exercise.py
@@ -0,0 +1,19 @@
+# REMEMBER, REFACTORING SHOULD NOT AFFECT THE CODE'S BEHAVIOUR
+
+
+# I want to print all numbers between 1 and 100. As you can see this is effort. Refactor this.
+ 
+print "1, 2, 3, 4, 5, 6, 7, 8, 9, 10......"
+
+# I want all even numbers between 1 and 100. 
+
+print '2, 4, 6, 8, 10......'
+
+# Print All numbers between 1 and 500 backwards only if they are divisible by 7 on the same line
+
+print '497 490 483 476 469 462 455 448 441 434 427 420 413 406 399 392 385 378 371 364' 
+print '357 350 343 336 329 322 315 308 301 294 287 280 273 266 259 252 245 238 231 224 217 210' 
+print '203 196 189 182 175 168 161 154 147 140 133 126 119 112 105 98 91 84 77 70 63 56 49 42'
+print '35 28 21 14 7'
+
+
